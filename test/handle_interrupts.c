2f25aad146627918258c9e943c264498c627d667
Daniel Paz <alu0101041735@ull.edu.es>
2020-11-26 12:30:30 +0000

    Feature started

---
+++ test/handle_interrupts.c
@@ -0,0 +1,18 @@
+
+/* *******************************************
+ * Libreria de E/S
+
+ Daniel Paz Marcos
+ David Martín Martín
+ Universidad de La Laguna
+
+ ******************************************* */
+
+#include <stdio.h>
+#include <types.h>
+#include <sys/param.h>
+#include <sys/interrupts.h>
+#include <sys/sio.h>
+#include <sys/locks.h>
+
+
