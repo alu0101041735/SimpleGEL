c9278d0749163e69cc2af093bf9eaf8b77d2c885
0x444d4d <alu0101053318@ull.edu.es>
2020-11-27 22:55:55 +0000

    Implement gpio_read_port

--- lib/gpio_rw.c
+++ lib/gpio_rw.c
@@ -5,9 +5,68 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define WRITE_ENABLED_PORT 0XFF
+#define READ_ENABLED_PORT 0X00
 
-unsigned char gpio_read_port(unsigned char port) {
-	unsigned char data = _io_ports[port];
+typedef struct Optional {
+	char is_valid;
+	unsigned char data;
+} Optional;
+
+Optional gpio_read_port(int port) {
+	Optional data;
+	if (port == M6812_PORTCAN) {
+
+		data.is_valid = 0;
+		data.data = 0;
+		return data;
+	}
+
+	int read_mode_enabled;
+
+	switch (port) {
+		case M6812_PORTA:
+			read_mode_enabled = ~(_io_ports[M6812_DDRA] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTB:
+			read_mode_enabled = ~(_io_ports[M6812_DDRB] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTG:
+			read_mode_enabled = ~(_io_ports[M6812_DDRG] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTH:
+			read_mode_enabled = ~(_io_ports[M6812_DDRH] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTP:
+			read_mode_enabled = ~(_io_ports[M6812_DDRP] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTS:
+			read_mode_enabled = ~(_io_ports[M6812_DDRS] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTT:
+			read_mode_enabled = ~(_io_ports[M6812_DDRT] | READ_ENABLED_PORT);
+			break;
+		case M6812_PORTAD0:
+			read_mode_enabled = 1;
+			break;
+		case M6812_PORTAD1:
+			read_mode_enabled = 1;
+			break;
+		case M6812_PORTE:
+			read_mode_enabled = ~(_io_ports[M6812_DDRE] | READ_ENABLED_PORT);
+			break;
+		default:
+			read_mode_enabled = 0;
+			break;
+	}
+
+	if (read_mode_enabled) {
+		data.is_valid = 1;
+		data.data = _io_ports[port];
+	} else {
+		data.is_valid = 0;
+		data.data = 0;
+	}
+
 	return data;
 }
 
