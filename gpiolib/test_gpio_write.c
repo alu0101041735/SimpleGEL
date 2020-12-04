0b76fc921f224fc80c7acdc04b8dacec816708e8
0x444d4d <alu0101053318@ull.edu.es>
2020-12-04 12:14:45 +0000

    Remove unused variables

--- gpiolib/test_gpio_write.c
+++ gpiolib/test_gpio_write.c
@@ -4,13 +4,10 @@
 
 
 int main() {
-	unsigned char data_arr[8];
-	unsigned char data_full;
-	unsigned char data;	
-
 	gpio_pup_disable_(M6812_PORTE);
 
 	gpio_set_output_all_reg(SET_PIN_E);
+
 	gpio_write_pin(M6812_PORTE, 0, 1);
 	gpio_write_pin(M6812_PORTE, 1, 0);
 	gpio_write_pin(M6812_PORTE, 2, 1);
