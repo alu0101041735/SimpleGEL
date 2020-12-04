268a16bd2a3cc40bfa5f17f9449cd6f44c4c9547
Daniel <alu0101041735@ull.edu.es>
2020-12-04 09:44:37 +0000

    Fixed indentation

--- lib/gpio.c
+++ lib/gpio.c
@@ -14,7 +14,7 @@
  * */
 void gpio_set_output(int reg, int port)
 {
-	_io_ports[reg] |= 1UL << port;
+  _io_ports[reg] |= 1UL << port;
 }
 
 /**
@@ -22,7 +22,7 @@ void gpio_set_output(int reg, int port)
  * */
 void gpio_set_output_all_reg(int reg)
 {
-	_io_ports[reg] |= 0xFF;
+  _io_ports[reg] |= 0xFF;
 }
 
 /**
@@ -30,7 +30,7 @@ void gpio_set_output_all_reg(int reg)
  * */
 void gpio_set_input(int reg, int port)
 {
-	_io_ports[reg] &= ~(1UL << port); 
+  _io_ports[reg] &= ~(1UL << port); 
 }
 
 
@@ -39,7 +39,7 @@ void gpio_set_input(int reg, int port)
  * */
 void gpio_set_input_all_reg(int reg)
 {
-	_io_ports[reg] &= 0x00;
+  _io_ports[reg] &= 0x00;
 }
 
 
@@ -50,11 +50,11 @@ void (*interrupt[])(void) = {SR_G, SR_H};
 
 
 /**
- * Función que activa un pin de interrupción, pasándole un registro y un número de puerto, definidos en la librería
+ * Función que activa un pin de interrupción 
  * */
 void gpio_set_interrupt_pin(int reg, int port)
 {
-	_io_ports[reg] |= 1UL << port;
+  _io_ports[reg] |= 1UL << port;
 }
 
 /**
@@ -62,7 +62,7 @@ void gpio_set_interrupt_pin(int reg, int port)
  * */
 void gpio_reset_interrupt_pin(int reg, int port)
 {
-	_io_ports[reg] &= ~(1UL << port); 
+  _io_ports[reg] &= ~(1UL << port); 
 }
 
 /**
@@ -70,7 +70,7 @@ void gpio_reset_interrupt_pin(int reg, int port)
  * */
 void gpio_reset_all_pins(int reg)
 {
-	_io_ports[reg] &= 0x00; 
+  _io_ports[reg] &= 0x00; 
 }
 
 /**
@@ -78,7 +78,7 @@ void gpio_reset_all_pins(int reg)
  * */
 void gpio_set_interrupt_all_pins(int reg)
 {
-    _io_ports[reg] |= 0xFF;
+  _io_ports[reg] |= 0xFF;
 }
 
 
@@ -88,81 +88,81 @@ void gpio_set_interrupt_all_pins(int reg)
  * */
 void __attribute__((interrupt)) vi_kwgh(void)
 {
-	
-	int reg;
-	int port_g;
-	int port;
 
-	if (_io_ports[M6812_KWIFG] != 0x00) {
+  int reg;
+  int port_g;
+  int port;
 
-		port_g = 0;
-		reg = _io_ports[M6812_KWIFG];
+  if (_io_ports[M6812_KWIFG] != 0x00) {
 
-	}
+    port_g = 0;
+    reg = _io_ports[M6812_KWIFG];
 
-	if (_io_ports[M6812_KWIFH] != 0x00) {
+  }
 
-		port_g = 1;
-		reg = _io_ports[M6812_KWIFH];
+  if (_io_ports[M6812_KWIFH] != 0x00) {
 
-	}
+    port_g = 1;
+    reg = _io_ports[M6812_KWIFH];
 
-	switch (reg) {
+  }
 
-		case (0x01):
-			serial_print("port0\n");
-			port = 0;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x02):
-			serial_print("port1\n");
-			port = 1;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x04):
-			serial_print("port2\n");
-			port = 2;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x08):
-			serial_print("port3\n");
-			port = 3;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x10):
-			serial_print("port4\n");
-			port = 4;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x20):
-			serial_print("port5\n");
-			port = 5;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x40):
-			serial_print("port6\n");
-			port = 6;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
-		case (0x80):
-			serial_print("port7\n");
-			port = 7;
-			_io_ports[reg] &= ~(1UL << port); 
-			break;
+  switch (reg) {
 
-		default:
-			port = -1;
-			break;
-	}
+    case (0x01):
+      serial_print("port0\n");
+      port = 0;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x02):
+      serial_print("port1\n");
+      port = 1;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x04):
+      serial_print("port2\n");
+      port = 2;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x08):
+      serial_print("port3\n");
+      port = 3;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x10):
+      serial_print("port4\n");
+      port = 4;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x20):
+      serial_print("port5\n");
+      port = 5;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x40):
+      serial_print("port6\n");
+      port = 6;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
+    case (0x80):
+      serial_print("port7\n");
+      port = 7;
+      _io_ports[reg] &= ~(1UL << port); 
+      break;
 
-	char c_port = (char) port;
-	if (port_g == 0) {
+    default:
+      port = -1;
+      break;
+  }
 
-                interrupt[SR_G]();
-	}
-	else {
-                interrupt[SR_H]();
-	}
+  char c_port = (char) port;
+  if (port_g == 0) {
+
+    interrupt[SR_G]();
+  }
+  else {
+    interrupt[SR_H]();
+  }
 
 
 }
@@ -172,36 +172,36 @@ void __attribute__((interrupt)) vi_kwgh(void)
  * Function to enable pullup on a register
  * */
 int gpio_pup_enable(int reg) {
-	int special_port = ((reg == M6812_PORTP) ||
-											(reg == M6812_PORTS) || 
-											(reg == M6812_PORTT) || 
-											(reg == M6812_PORTCAN));
+  int special_port = ((reg == M6812_PORTP) ||
+      (reg == M6812_PORTS) || 
+      (reg == M6812_PORTT) || 
+      (reg == M6812_PORTCAN));
 
 
-	if (!special_port) {
-		//Enable pullup for ports A, B, E, G, H
-		_io_ports[M6812_PUCR] |= reg;
+  if (!special_port) {
+    //Enable pullup for ports A, B, E, G, H
+    _io_ports[M6812_PUCR] |= reg;
 
-	} else if (reg == M6812_PORTP){
-		//Enable pup for port P
-		_io_ports[M6812_PWCTL] |= 0x01 << 1;
+  } else if (reg == M6812_PORTP){
+    //Enable pup for port P
+    _io_ports[M6812_PWCTL] |= 0x01 << 1;
 
-	} else if (reg == M6812_PORTS){
-		//Enable pup for port S
-		unsigned char bits = _io_ports[M6812_PURDS];
-		bits |= 0x07;
-		_io_ports[M6812_PURDS] = bits;
+  } else if (reg == M6812_PORTS){
+    //Enable pup for port S
+    unsigned char bits = _io_ports[M6812_PURDS];
+    bits |= 0x07;
+    _io_ports[M6812_PURDS] = bits;
 
-	} else if (reg == M6812_PORTT){
-		//Enable pup for port T
-		_io_ports[M6812_TMSK2] |= 0x01 << 5;
+  } else if (reg == M6812_PORTT){
+    //Enable pup for port T
+    _io_ports[M6812_TMSK2] |= 0x01 << 5;
 
-	} else if (reg == M6812_PORTCAN){
-		_io_ports[M6812_PCTLCAN] |= 0x02;
-	} else {
-		return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+  } else if (reg == M6812_PORTCAN){
+    _io_ports[M6812_PCTLCAN] |= 0x02;
+  } else {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
 
 
@@ -209,36 +209,36 @@ int gpio_pup_enable(int reg) {
  * Function to disable pullup on a register
  * */
 int gpio_pup_disable_(int reg) {
-	int special_port = ((reg == M6812_PORTP) ||
-											(reg == M6812_PORTS) || 
-											(reg == M6812_PORTT) || 
-											(reg == M6812_PORTCAN));
+  int special_port = ((reg == M6812_PORTP) ||
+      (reg == M6812_PORTS) || 
+      (reg == M6812_PORTT) || 
+      (reg == M6812_PORTCAN));
 
 
-	if (!special_port) {
-		//Enable pullup for ports A, B, E, G, H
-		_io_ports[M6812_PUCR] &= ~reg;
+  if (!special_port) {
+    //Enable pullup for ports A, B, E, G, H
+    _io_ports[M6812_PUCR] &= ~reg;
 
-	} else if (reg == M6812_PORTP){
-		//Enable pup for port P
-		_io_ports[M6812_PWCTL] &= ~(0x01 << 1);
+  } else if (reg == M6812_PORTP){
+    //Enable pup for port P
+    _io_ports[M6812_PWCTL] &= ~(0x01 << 1);
 
-	} else if (reg == M6812_PORTS){
-		//Enable pup for port S
-		unsigned char bits = _io_ports[M6812_PURDS];
-		bits &= ~0x07;
-		_io_ports[M6812_PURDS] = bits;
+  } else if (reg == M6812_PORTS){
+    //Enable pup for port S
+    unsigned char bits = _io_ports[M6812_PURDS];
+    bits &= ~0x07;
+    _io_ports[M6812_PURDS] = bits;
 
-	} else if (reg == M6812_PORTT){
-		//Enable pup for port T
-		_io_ports[M6812_TMSK2] &= ~(0x01 << 5);
+  } else if (reg == M6812_PORTT){
+    //Enable pup for port T
+    _io_ports[M6812_TMSK2] &= ~(0x01 << 5);
 
-	} else if (reg == M6812_PORTCAN){
-		_io_ports[M6812_PCTLCAN] &= ~0x02;
-	} else {
-		return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+  } else if (reg == M6812_PORTCAN){
+    _io_ports[M6812_PCTLCAN] &= ~0x02;
+  } else {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
 
 
@@ -246,59 +246,59 @@ int gpio_pup_disable_(int reg) {
  * Función para escribir un pin
  * */
 void gpio_write_pin(int port, unsigned char pin, unsigned char data) {
-	unsigned char direction = 0;
-	int direction_port = 0;
-	unsigned short is_read_only = (port == M6812_PORTAD0) ||
-																(port == M6812_PORTAD1);
+  unsigned char direction = 0;
+  int direction_port = 0;
+  unsigned short is_read_only = (port == M6812_PORTAD0) ||
+    (port == M6812_PORTAD1);
 
-	//If pin not in range return empy optional.
-	if (!(pin < 8 && pin >= 0)) {
-		return;
-	}
+  //If pin not in range return empy optional.
+  if (!(pin < 8 && pin >= 0)) {
+    return;
+  }
 
-	//If port is read only avoid further checks.
-	if (is_read_only) {
-		return;
-	}
+  //If port is read only avoid further checks.
+  if (is_read_only) {
+    return;
+  }
 
-	switch (port) {
-		case M6812_PORTA:
-			direction_port = M6812_DDRA;
-			break;
-		case M6812_PORTB:
-			direction_port = M6812_DDRB;
-			break;
-		case M6812_PORTE:
-			direction_port = M6812_DDRE;
-			break;
-		case M6812_PORTG:
-			direction_port = M6812_DDRG;
-			break;
-		case M6812_PORTH:
-			direction_port = M6812_DDRH;
-			break;
-		case M6812_PORTP:
-			direction_port = M6812_DDRP;
-			break;
-		case M6812_PORTS:
-			direction_port = M6812_DDRS;
-			break;
-		case M6812_PORTT:
-			direction_port = M6812_DDRT;
-			break;
-		case M6812_PORTCAN:
-			direction_port = M6812_DDRCAN;
-			break;
-	}
+  switch (port) {
+    case M6812_PORTA:
+      direction_port = M6812_DDRA;
+      break;
+    case M6812_PORTB:
+      direction_port = M6812_DDRB;
+      break;
+    case M6812_PORTE:
+      direction_port = M6812_DDRE;
+      break;
+    case M6812_PORTG:
+      direction_port = M6812_DDRG;
+      break;
+    case M6812_PORTH:
+      direction_port = M6812_DDRH;
+      break;
+    case M6812_PORTP:
+      direction_port = M6812_DDRP;
+      break;
+    case M6812_PORTS:
+      direction_port = M6812_DDRS;
+      break;
+    case M6812_PORTT:
+      direction_port = M6812_DDRT;
+      break;
+    case M6812_PORTCAN:
+      direction_port = M6812_DDRCAN;
+      break;
+  }
 
-	direction = _io_ports[direction_port] & pin;
-	if (direction != 1) {
-		return;
-	} else {
-		char register_cpy = _io_ports[port];
-		register_cpy = (register_cpy & (~pin));
-		_io_ports[port] = register_cpy | pin;
-	}
+  direction = _io_ports[direction_port] & pin;
+  if (direction != 1) {
+    return;
+  } else {
+    char register_cpy = _io_ports[port];
+    register_cpy = (register_cpy & (~pin));
+    _io_ports[port] = register_cpy | pin;
+  }
 }
 
 
@@ -306,65 +306,65 @@ void gpio_write_pin(int port, unsigned char pin, unsigned char data) {
  * Función para leer un pin
  * */
 Optional gpio_read_pin(int port, unsigned char pin) {
-	Optional data;
-	data.is_valid = 0;
-	unsigned char direction = 0;
-	int direction_port = 0;
-	unsigned short is_read_only = (port == M6812_PORTAD0) ||
-																(port == M6812_PORTAD1);
+  Optional data;
+  data.is_valid = 0;
+  unsigned char direction = 0;
+  int direction_port = 0;
+  unsigned short is_read_only = (port == M6812_PORTAD0) ||
+    (port == M6812_PORTAD1);
 
-	//If pin not in range return empy optional.
-	if (!(pin < 8 && pin >= 0)) {
-		return data;
-	}
+  //If pin not in range return empy optional.
+  if (!(pin < 8 && pin >= 0)) {
+    return data;
+  }
 
-	//If port is read only avoid further checks.
-	if (is_read_only) {
-		data.is_valid = 0;
-		data.data = _io_ports[port] & pin;
-		return data;
-	}
+  //If port is read only avoid further checks.
+  if (is_read_only) {
+    data.is_valid = 0;
+    data.data = _io_ports[port] & pin;
+    return data;
+  }
 
-	switch (port) {
-		case M6812_PORTA:
-			direction_port = M6812_DDRA;
-			break;
-		case M6812_PORTB:
-			direction_port = M6812_DDRB;
-			break;
-		case M6812_PORTE:
-			direction_port = M6812_DDRE;
-			break;
-		case M6812_PORTG:
-			direction_port = M6812_DDRG;
-			break;
-		case M6812_PORTH:
-			direction_port = M6812_DDRH;
-			break;
-		case M6812_PORTP:
-			direction_port = M6812_DDRP;
-			break;
-		case M6812_PORTS:
-			direction_port = M6812_DDRS;
-			break;
-		case M6812_PORTT:
-			direction_port = M6812_DDRT;
-			break;
-		case M6812_PORTCAN:
-			direction_port = M6812_DDRCAN;
-			break;
-		default:
-			return data;
-	}
+  switch (port) {
+    case M6812_PORTA:
+      direction_port = M6812_DDRA;
+      break;
+    case M6812_PORTB:
+      direction_port = M6812_DDRB;
+      break;
+    case M6812_PORTE:
+      direction_port = M6812_DDRE;
+      break;
+    case M6812_PORTG:
+      direction_port = M6812_DDRG;
+      break;
+    case M6812_PORTH:
+      direction_port = M6812_DDRH;
+      break;
+    case M6812_PORTP:
+      direction_port = M6812_DDRP;
+      break;
+    case M6812_PORTS:
+      direction_port = M6812_DDRS;
+      break;
+    case M6812_PORTT:
+      direction_port = M6812_DDRT;
+      break;
+    case M6812_PORTCAN:
+      direction_port = M6812_DDRCAN;
+      break;
+    default:
+      return data;
+  }
 
-	direction = _io_ports[direction_port] & pin;
-	if (direction != 0) {
-		return data;
-	}
-	
-	data.data = _io_ports[port] && pin;
-	data.is_valid = 1;
-	return data;
+  direction = _io_ports[direction_port] & pin;
+  if (direction != 0) {
+    return data;
+  }
+
+  data.data = _io_ports[port] && pin;
+  data.is_valid = 1;
+  return data;
 }
 
 
@@ -372,61 +372,61 @@ Optional gpio_read_pin(int port, unsigned char pin) {
  * Función para leer un puerto
  * */
 Optional gpio_read_port(int port) {
-	Optional data;
-	if (port == M6812_PORTCAN) {
+  Optional data;
+  if (port == M6812_PORTCAN) {
 
-		data.is_valid = 0;
-		data.data = 0;
-		return data;
-	}
+    data.is_valid = 0;
+    data.data = 0;
+    return data;
+  }
 
-	unsigned short read_mode_enabled;
+  unsigned short read_mode_enabled;
 
-	switch (port) {
-		case M6812_PORTA:
-			read_mode_enabled = ~(_io_ports[M6812_DDRA] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTB:
-			read_mode_enabled = ~(_io_ports[M6812_DDRB] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTG:
-			read_mode_enabled = ~(_io_ports[M6812_DDRG] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTH:
-			read_mode_enabled = ~(_io_ports[M6812_DDRH] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTP:
-			read_mode_enabled = ~(_io_ports[M6812_DDRP] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTS:
-			read_mode_enabled = ~(_io_ports[M6812_DDRS] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTT:
-			read_mode_enabled = ~(_io_ports[M6812_DDRT] | READ_ENABLED_PORT);
-			break;
-		case M6812_PORTAD0:
-			read_mode_enabled = 1;
-			break;
-		case M6812_PORTAD1:
-			read_mode_enabled = 1;
-			break;
-		case M6812_PORTE:
-			read_mode_enabled = ~(_io_ports[M6812_DDRE] | READ_ENABLED_PORT);
-			break;
-		default:
-			read_mode_enabled = 0;
-			break;
-	}
+  switch (port) {
+    case M6812_PORTA:
+      read_mode_enabled = ~(_io_ports[M6812_DDRA] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTB:
+      read_mode_enabled = ~(_io_ports[M6812_DDRB] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTG:
+      read_mode_enabled = ~(_io_ports[M6812_DDRG] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTH:
+      read_mode_enabled = ~(_io_ports[M6812_DDRH] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTP:
+      read_mode_enabled = ~(_io_ports[M6812_DDRP] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTS:
+      read_mode_enabled = ~(_io_ports[M6812_DDRS] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTT:
+      read_mode_enabled = ~(_io_ports[M6812_DDRT] | READ_ENABLED_PORT);
+      break;
+    case M6812_PORTAD0:
+      read_mode_enabled = 1;
+      break;
+    case M6812_PORTAD1:
+      read_mode_enabled = 1;
+      break;
+    case M6812_PORTE:
+      read_mode_enabled = ~(_io_ports[M6812_DDRE] | READ_ENABLED_PORT);
+      break;
+    default:
+      read_mode_enabled = 0;
+      break;
+  }
 
-	if (read_mode_enabled) {
-		data.is_valid = 1;
-		data.data = _io_ports[port];
-	} else {
-		data.is_valid = 0;
-		data.data = 0;
-	}
+  if (read_mode_enabled) {
+    data.is_valid = 1;
+    data.data = _io_ports[port];
+  } else {
+    data.is_valid = 0;
+    data.data = 0;
+  }
 
-	return data;
+  return data;
 }
 
 
@@ -434,68 +434,68 @@ Optional gpio_read_port(int port) {
  * Función para leer un puerto
  * */
 char gpio_write_port(int port, char data) {
-	//Only allow to write to ports with all pins able to read
-	int is_read_only = ((port == M6812_PORTAD0) ||
-											(port == M6812_PORTAD1) ||
-											(port == M6812_PORTE) ||
-											(port == M6812_PORTCAN));
+  //Only allow to write to ports with all pins able to read
+  int is_read_only = ((port == M6812_PORTAD0) ||
+      (port == M6812_PORTAD1) ||
+      (port == M6812_PORTE) ||
+      (port == M6812_PORTCAN));
 
-	if (is_read_only) {
-		return EXIT_FAILURE;
-	}
+  if (is_read_only) {
+    return EXIT_FAILURE;
+  }
 
-	//Check that the port is in write mode, if no return EXIT_FAILURE
-	int is_write_enabled = 0;
-	switch (port) {
-		case M6812_PORTA:
-			if (_io_ports[M6812_DDRA] & WRITE_ENABLED_PORT ) {
-				is_write_enabled = 1;
-			} else {
-			}
-			break;
+  //Check that the port is in write mode, if no return EXIT_FAILURE
+  int is_write_enabled = 0;
+  switch (port) {
+    case M6812_PORTA:
+      if (_io_ports[M6812_DDRA] & WRITE_ENABLED_PORT ) {
+        is_write_enabled = 1;
+      } else {
+      }
+      break;
 
-		case M6812_PORTG:
-			if (_io_ports[M6812_DDRG] & WRITE_ENABLED_PORT ) {
-				is_write_enabled = 1;
-			} else {
-			}
-			break;
-			
-		case M6812_PORTP:
-			if (_io_ports[M6812_DDRP] & WRITE_ENABLED_PORT ) {
-				is_write_enabled = 1;
-			} else {
-			}
-			break;
-			
-		case M6812_PORTH:
-			if (_io_ports[M6812_DDRH] & WRITE_ENABLED_PORT ) {
-				is_write_enabled = 1;
-			} else {
-			}
-			break;
-			
-		case M6812_PORTS:
-			if (_io_ports[M6812_DDRS] & WRITE_ENABLED_PORT ) {
-				is_write_enabled = 1;
-			} else {
-			}
-			break;
-			
-		case M6812_PORTT:
-			if (_io_ports[M6812_DDRT] & WRITE_ENABLED_PORT ) {
-				is_write_enabled = 1;
-			} else {
-			}
-			break;
-			
-		default:
-			return EXIT_FAILURE;
-	}
+    case M6812_PORTG:
+      if (_io_ports[M6812_DDRG] & WRITE_ENABLED_PORT ) {
+        is_write_enabled = 1;
+      } else {
+      }
+      break;
 
-	if (!is_write_enabled) {
-		return EXIT_FAILURE;
-	}
-	_io_ports[port] = data;
-	return EXIT_SUCCESS;
+    case M6812_PORTP:
+      if (_io_ports[M6812_DDRP] & WRITE_ENABLED_PORT ) {
+        is_write_enabled = 1;
+      } else {
+      }
+      break;
+
+    case M6812_PORTH:
+      if (_io_ports[M6812_DDRH] & WRITE_ENABLED_PORT ) {
+        is_write_enabled = 1;
+      } else {
+      }
+      break;
+
+    case M6812_PORTS:
+      if (_io_ports[M6812_DDRS] & WRITE_ENABLED_PORT ) {
+        is_write_enabled = 1;
+      } else {
+      }
+      break;
+
+    case M6812_PORTT:
+      if (_io_ports[M6812_DDRT] & WRITE_ENABLED_PORT ) {
+        is_write_enabled = 1;
+      } else {
+      }
+      break;
+
+    default:
+      return EXIT_FAILURE;
+  }
+
+  if (!is_write_enabled) {
+    return EXIT_FAILURE;
+  }
+  _io_ports[port] = data;
+  return EXIT_SUCCESS;
 }
